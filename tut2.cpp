3411668c861ad47d97b0c82ca98e493aa78f83e7
Manya24singh <manya.23bce11577@vitbhopal.ac.in>
2026-05-07 13:22:03 +0530

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,46 @@
+// Basic Structure of a C++ Program (explain)
+
+cpp
+#include<iostream>
+
+int main() {
+    std::cout << "Hello World";
+    return 0;
+}
+
+
+ /*Explanation
+
+1.#include<iostream>
+
+   * Adds the iostream library.
+   * Used for taking input and showing output.
+
+2. Blank Line
+
+   * Empty lines do not affect the program.
+   * Compiler ignores them.
+
+3. int main() {
+
+   `main()` is the starting function of the program.
+    `int` means it returns an integer value.
+    `{` starts the function.
+
+4. std::cout << "Hello World";
+
+    Used to display output on the screen.
+    `"Hello World"` is the text printed.
+   *`;` ends the statement.
+
+5. return 0;
+
+    Ends the program successfully.
+    `0` means no error.
+
+6. `}`
+
+    Closes the `main()` function.
+
+Output
+Hello World
