5171738e0ab9efdf9a47f4632fc0a8b5f3409b6a
Manya24singh <manya.23bce11577@vitbhopal.ac.in>
2026-05-21 10:17:02 +0530

    Add files via upload

---
+++ tut4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int a= 4;
+    int b= 6;
+    // int a= 12 , b= 15 ( aise bhi ek sath values likh sakte h)
+    float pi = 3.14;
+    char c = 'd';
+    bool is_ true = true ;
+     cout << 'this is tutorial 4. here the value of a is ' <<a<< ' the value of b is ' << b;
+     cout << "the value of pi is "<<pi;
+     cout << "the value of c is "<< c;
+     cout<<" the value of bool is "<< bool;
+
+
+    /* cout << 'this is tutorial 4.\n here the value of a is ' <<a<< '.\n the value of b is ' << b;
+     cout << "the value of pi is "<<pi;*/ 
+
+
+
+    return 0;
+}
